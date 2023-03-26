edf30fc664e32c760e4e01a17d947dc0d46d3a12
Irakli metonidze <imetonidze@newage.io>
2023-03-26 15:11:05 +0400

    ....
    ...

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sumArrIElements = (arr) => {
+const sumArrElements = (arr) => {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
@@ -6,5 +6,5 @@ const sumArrIElements = (arr) => {
   return sum;
 };
 
-console.log(sumArrIElements([12, 33, 44, 55, 33, 443, 44])); // 664
-console.log(sumArrIElements([1, 2, 3, 4, 5])); // 15
+console.log(sumArrElements([12, 33, 44, 55, 33, 443, 44])); // 664
+console.log(sumArrElements([1, 2, 3, 4, 5])); // 15
